6354e41fe39da80f8dda646286e7f80a5bfa39f5
Ruan Araujo <103197582+ruantes@users.noreply.github.com>
2025-04-07 20:45:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var titulo = document.getElementById("titulo")
 var btn = document.querySelector("#btn")
 
 function disparaAlerta(){
-    alert("Ruan")
-    titulo.innerText = "Ruan"
+    var body = document.querySelector("body")
+body.classList.toggle("dark-mode")
+
+if (btn.innerText == "Ativar"){
+    btn.innerText = "Desativar"
+}else {
+    btn.innerText = "Ativar"
+}
 }
 
 btn.addEventListener("click", disparaAlerta)
